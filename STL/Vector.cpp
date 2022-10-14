f15855d72621fefc691fa8ad772fb7dcfc3affea
abirsgh <98998279+abirsgh@users.noreply.github.com>
2022-10-15 00:50:38 +0600

    Create Vector.cpp

---
+++ STL/Vector.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> v;
+
+void prnt() {
+	for(int i = 0; i < (int)v.size(); i++) {
+		cout << v[i] << " ";
+	}cout << endl;
+}
+
+int main(){
+	for(int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}
+	sort(v.rbegin(), v.rend());
+	for(int i = 0; i < v.size(); i++) {
+		cout << v[i] << " ";
+	}cout << endl;
+	int mx = *min_element(v.begin(), v.end());
+	cout << INT_MIN << endl;
+	cout << INT_MAX << endl;
+	bool ok;
+	ok = is_sorted(v.begin(), v.end());
+	cout << ok << endl;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(1);
+	v.push_back(4);
+	v.push_back(10);
+	v.push_back(11);
+	prnt();
+	v.pop_back();
+	prnt();
+	cout << v.front() << "\n";
+}
