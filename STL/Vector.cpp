7fbb1db2742ab306d5748f0cf81f230570dee2e3
Abir Hasan <98998279+h-abir@users.noreply.github.com>
2022-10-16 02:04:17 +0600

    Update Vector.cpp

--- STL/Vector.cpp
+++ STL/Vector.cpp
@@ -32,4 +32,8 @@ int main(){
 	v.pop_back();
 	prnt();
 	cout << v.front() << "\n";
+	//Removes the element present at position.  
+	v.erase(v.begin()+4); //(erases the fifth element of the vector v)
+	//Removes the elements in the range from start to end inclusive of the start and exclusive of the end.
+	v.erase(v.begin()+2,v.begin()+5); //(erases all the elements from the third element to the fifth element.)
 }
