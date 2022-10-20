789951845eace8c648a17d49487d862b2af587d5
Abir Hasan <98998279+h-abir@users.noreply.github.com>
2022-10-20 18:43:11 +0600

    Update string.cpp

--- STL/string.cpp
+++ STL/string.cpp
@@ -5,12 +5,17 @@ int main() {
 
 	string s = "aaAsca caba";
 	string ss = s;
-	reverse(ss.begin(), ss.end());
+	reverse(ss.begin(), ss.end());  //reverse a string 
 	if(s == ss)cout << "Palindrom\n";
 	else cout << "NO" << "\n";
 	cout << s.substr(2, 6);
 
 	string s = "aBcAcnd";
-	transform(s.begin(), s.end(), s.begin(), ::tolower);
-	cout << s << endl;
+	transform(s.begin(), s.end(), s.begin(), ::tolower);  //transform to lower
+	transform(s.begin(),s.end(),s.begin(), ::toupper);  //transform to upper
+	set<char> st;
+	for(int i = 0; i < n; i++){
+	    st.insert(s[i]);
+	}
+	cout << (st.size() == 26 ? "YES" : "NO") << "\n";
 }
