8f2bc5768743d3c05a27de51e85650d595af7442
Abir Hasan <98998279+h-abir@users.noreply.github.com>
2022-11-04 18:36:16 +0600

    Update Vector.cpp

--- STL/Vector.cpp
+++ STL/Vector.cpp
@@ -13,21 +13,26 @@ int main(){
 	for(int i = 0; i < v.size(); i++) {
 		cout << v[i] << " ";
 	}
-	sort(v.rbegin(), v.rend());
-	for(int i = 0; i < v.size(); i++) {
-		cout << v[i] << " ";
-	}cout << endl;
-	int mn = *min_element(v.begin(), v.end());
+	
+	sort(v.rbegin(), v.rend());      //Sort a Vector
+
+	int mn = *min_element(v.begin(), v.end()); //Finding minimum element
+	int mx = *max_element(v.begin(), v.end()); //Finding maximum element
+	
 	cout << INT_MIN << endl;
 	cout << INT_MAX << endl;
+	
 	bool ok;
 	ok = is_sorted(v.begin(), v.end());
 	cout << ok << endl;
-	v.push_back(1);  //Add a value in the last
-	v.pop_back();    //Delete last value
+	
+	v.push_back(1);  	//Add a value in the last
+	v.pop_back();    	//Delete last value
 	printVec(v);
 	cout << v.front() << "\n";  //Removes the element present at position.  
+	
 	v.erase(v.begin()+4); //(erases the fifth element of the vector v)
+	
 	//Removes the elements in the range from start to end inclusive of the start and exclusive of the end.
 	v.erase(v.begin()+2,v.begin()+5); //(erases all the elements from the third element to the fifth element.)
 }
