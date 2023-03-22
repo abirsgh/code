5a9dd0996b4797ad79e3e81bc9d53cec99b3e347
Abir Hasan <98998279+h-abir@users.noreply.github.com>
2023-03-23 00:57:16 +0600

    Update General Jinishpati.cpp

--- General Jinishpati.cpp
+++ General Jinishpati.cpp
@@ -9,4 +9,11 @@ int main(){
       // pi value 
             pi = acos(0.0);
       
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        printf("%d is a leap year\n", year);
+    }
+     else {
+        printf("%d is not a leap year\n", year);
+    }
+      
 }      
